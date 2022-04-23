2343b4eb74157d1ac9af4c9863b20419899bb0ad
wrjh201 <104253820+wrjh201@users.noreply.github.com>
2022-04-23 15:32:47 +0100

    Improvements. Fixed get-result (I think).

--- wordle.lisp
+++ wordle.lisp
@@ -7,6 +7,9 @@
   (every (lambda (line) (= (length line) 5)) list)
   )
 
+(defparameter *words* (sort (load-words #P"pos_words.txt") #'string<) 
+  "List of 5 letter words that are valid guesses in Wordle.")
+
 (defun nloops (lst n)
   ;; https://codegolf.stackexchange.com/a/165665
   (if (< n 1)
@@ -17,55 +20,67 @@
                 nconc (loop for b in (nloops lst (1- n))
                             collect (cons a b))))))
 
-(defun word-does-match (guess word result)
-  "Correct if the guess, with the result is a candidate word. "
-  (every (lambda (l1 l2 res)
-	   (ecase res
-	     (green (char-equal l1 l2))
-	     (yellow (some (lambda (x) (char-equal x l1)) word))
-	     (grey (notany (lambda (x) (char-equal x l1)) word))
-	     ))
-	 guess word result))
-	   
+;; (defun get-result (guess word)
+;;   "Get the wordle result of GUESS from WORD."
+;;   ;; TODO: This probably doesn't handle repeated words properly.
+;;   (loop for letter across guess
+;; 	for idx from 0
+;; 	where occured = ()
+;; 	collect (cond ((char= letter (char word idx)) 'green)
+;; 		      ((some (lambda (x) (char= letter x)) word) 'yellow)
+;; 		      (t 'grey))))
 
-(defun get-possible-words (guess result &optional (words *words*))
-  (loop for word in words
-	if (word-does-match guess word result)
-	  collect word))
-
-(defun expected-info (word &optional (words *words*))
-  "Take a guess and return the expected information gathered from it."
-  (let ((total-words (length words)))
-    (loop for result in (nloops '(green yellow grey) 5)
-	  sum (let ((px (/ (length (get-possible-words word result)) total-words)))
-		(if (= 0 px)
-		    0
-		    (* px (log (/ 1 px) 2))
-		)))
-    ))
-
-(defparameter *words* (load-words #P"words.txt"))
-
-(defun information-from-probability (p)
-  "Get the amount of information in bits from the probability."
-  (- (log p 2))
+(defun result-green-pass (guess word)
+  "Only set matching letters green. Leave the rest grey."
+  (loop for lg across guess
+	for lw across word
+	collect (cons lg (if (char= lg lw) 'green 'grey)))
   )
 
-(defun generate-letter-result (letter pos answer)
-  (cond ((char= letter (char answer pos)) 'green)
-	((find letter answer) 'yellow)
-	(t 'grey))
+(defun get-result (guess word)
+  (loop with result = (result-green-pass guess word)
+	for r in result
+	do (print result)
+	unless (eq (cdr r) 'green)
+	  do (when (some (lambda (lw rs) (and (char= (car r) lw)
+					      (eq rs 'grey)))
+			 word
+			 (map 'list #'cdr result))
+	       (setf (cdr r) 'yellow))
+	     finally (return result)
+	)
   )
+
+
+;; result-matches-p
+;; TODO: write a function that takes a word, a guess and a result and returns T
+;;       if the word is a possible candidate for the answer given the guess and
+;;       its result.
 
 (defun wordle-emoji (result)
-  (ecase result
-    (green "🟩")
-    (yellow "🟨")
-    (grey "⬛"))
+  (cond ((symbolp result) (ecase result
+			    (green #\🟩)
+			    (yellow #\🟨)
+			    (grey #\⬛)))
+	((listp result) (map 'string 'wordle-emoji result))))
+
+(defun possible-words (guess result &optional (words *words*))
+  "Return a list of possible words matching result with guess from words."
+  (loop for word in words
+	if (result-matches-p guess word result)
+	  collect word)
   )
 
-(defun generate-score (guess answer)
-  (loop for letter across guess
-	for index from 0
-	collect (generate-letter-result letter index answer))
+(defun simple-px (guess result &optional (words *words*))
+  "Naive implementation of p(x). The probability of the pattern of guess with result occuring randomly."
+  (/ (length (possible-words guess result words))
+     (length words))
   )
+
+(defun expected-information (guess &optional (words *words*))
+  "The expected information from guessing GUESS from the word pool WORDS."
+  (loop for result in (nloops '(green yellow grey) 5)
+	for px = (simple-px guess result words)
+	unless (zerop px)
+	  sum (* px (- (log px 2)))
+  ))
